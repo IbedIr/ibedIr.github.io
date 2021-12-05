dca7cfad7af08bf74dbf3e938d286b69d35b5d55
IbedIr <94257916+IbedIr@users.noreply.github.com>
2021-12-05 04:22:00 -0300

    Update musicsData.js

--- player/assets/js/utils/musicsData.js
+++ player/assets/js/utils/musicsData.js
@@ -105,7 +105,7 @@ const musicsData = [
   },
   {
     musicFile:"Agnus Dei   Würdig ist das Lamm.mp3",
-    albumFile:"agnus-dei-deutchland.jpg",
+    albumFile:"agnus-dei-deutschland.jpg",
     name:"Agnus Dei - Deutsch (Multilanguage)",
     performer:"Würdig ist das Lamm",
   },
